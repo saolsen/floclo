e0eeabedc0d8a2771194079e7f2a16842b381836
Stephen Olsen <steve.olsen@gmail.com>
2014-01-02 14:00:33 -0500

    lein version

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Clojure evaluating flowdock bot."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/data.json "0.2.3"]
